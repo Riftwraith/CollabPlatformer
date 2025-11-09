31b89244c3420093862e20618248a7c2bb59acf6
Ivan <ivan.ho@cl.cam.ac.uk>
2025-11-09 15:10:43 +0000

    debug teleporter

--- scripts/DebugTeleporter.gd
+++ scripts/DebugTeleporter.gd
@@ -1,15 +1,34 @@
 extends Node
 class_name DebugTeleporter
 
-@export var teleport_locations: Array[Node2D]
+# todo: automatically get list of debug points?
+@export var teleport_locations: Array[DebugPoint]
+var parent: Node2D
+
+var prevKey: Key = KEY_NONE
+
+func _teleport_to(debugPoint: DebugPoint) -> void:
+	print_debug("teleporting to ", debugPoint.global_position)
+	parent.global_position = debugPoint.global_position
+	pass
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
+	parent = get_parent()
+	if teleport_locations.is_empty():
+		push_warning("teleport locations not assigned!")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	if Input.is_key_pressed(Key_CTRL):
-		if Input.is_anything_pressed():
-			pass
+func _process(_delta: float) -> void:
+	if Input.is_key_pressed(KEY_CTRL):
+		for n in range(teleport_locations.size()):
+			assert(n < 9, "debug tp only supports key_1 through key_9")
+			var key = KEY_1 + n as Key
+			if prevKey == key and !Input.is_key_pressed(prevKey):
+				# tp on key release
+				_teleport_to(teleport_locations[n])
+				prevKey = KEY_NONE
+			if Input.is_key_pressed(key):
+				prevKey = key
+				pass
+	pass
