399ef9f6c52e6b536dcb488a29781132eb18f091
Ivan <ivan.ho@cl.cam.ac.uk>
2025-11-09 14:48:19 +0000

    reorganized scenes and scripts

--- scripts/DebugTeleporter.gd
+++ scripts/DebugTeleporter.gd
@@ -10,4 +10,6 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	if Input.is_key_pressed(Key_CTRL):
+		if Input.is_anything_pressed():
 	pass
