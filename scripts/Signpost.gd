2390817d65183fe943eb224e97c446663d1bc435
Riftwraith <riftwraith9@gmail.com>
2025-12-06 00:41:55 +0000

    better signs

--- scripts/Signpost.gd
+++ scripts/Signpost.gd
@@ -1,18 +1,26 @@
+@tool
 extends Node2D
 class_name Signpost
 
-@export var text: String = ""
+@export_multiline var text: String = "":
+	set(value):
+		text = value
+		if is_instance_valid(text_label):
+			text_label.text = text
 @onready var sprite = $Sprite2D
 @onready var interactable = $Interactable
-@onready var text_panel = $TextPanel
-@onready var text_label = $TextPanel/Label
+@onready var text_panel = $MarginContainer
+@onready var text_label = $MarginContainer/MarginContainer/Label
 @onready var focus_label = $FocusLabel
+
+@onready var outline_sprite = $OutlineSprite
 
 var text_opened = false
 
 
 func _on_focus_start():
-	sprite.modulate = Color(2, 2, 2, 1)
+	#sprite.modulate = Color(2, 2, 2, 1)
+	outline_sprite.show()
 	if not text_opened:
 		focus_label.show()
 
@@ -20,7 +28,8 @@ func _on_focus_end():
 	if text_opened:
 		_close_text()
 	focus_label.hide()
-	sprite.modulate = Color(1, 1, 1, 1)
+	#sprite.modulate = Color(1, 1, 1, 1)
+	outline_sprite.hide()
 
 
 func _on_interact():
@@ -40,9 +49,10 @@ func _close_text():
 	_on_focus_start()
 
 func _ready():
-	text_panel.hide()
+	if not Engine.is_editor_hint():
+		text_panel.hide()
 	text_label.text = text
 	focus_label.hide()
-	
+	outline_sprite.hide()
 
  
--- scripts/gui.gd
+++ scripts/gui.gd
@@ -4,6 +4,7 @@ class_name gui
 @onready var collectable_gui = $CollectableGUI
 @onready var collectable_label = $CollectableGUI/CollectableLabel
 
+@onready var interact_hold_timer = $Timers/InteractHold
 
 func _ready():
 	collectable_gui.modulate = Color(1, 1, 1, 0)
@@ -12,9 +13,10 @@ func _process(_delta):
 	collectable_label.text = str(RoomManager.obtained_collectables) + " / " + str(RoomManager.total_collectables)
 	
 	if Input.is_action_just_pressed("interact"):
-		var tween = create_tween()
-		tween.tween_property(collectable_gui, "modulate", Color(1, 1, 1, 1), 0.2)
+		interact_hold_timer.start(0.1)
+
 	if Input.is_action_just_released("interact"):
+		interact_hold_timer.stop()
 		var tween = create_tween()
 		tween.tween_property(collectable_gui, "modulate", Color(1, 1, 1, 0), 0.2)
 
@@ -24,3 +26,9 @@ func flash_collectables():
 	await get_tree().create_timer(2.0).timeout 
 	tween = create_tween()
 	tween.tween_property(collectable_gui, "modulate", Color(1, 1, 1, 0), 0.2)
+
+
+func _on_interact_hold_timeout():
+	#show collectable GUI
+	var tween = create_tween()
+	tween.tween_property(collectable_gui, "modulate", Color(1, 1, 1, 1), 0.2)
