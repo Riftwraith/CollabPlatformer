3e370c7b93a04d89e5ce16e33cc6b4192bf2db81
Riftwraith <riftwraith9@gmail.com>
2026-01-26 21:51:14 +0000

    added like 3 comments to interactable

--- scripts/Objects/Interactable.gd
+++ scripts/Objects/Interactable.gd
@@ -5,11 +5,11 @@ signal focus_start
 signal focus_end
 signal interacted
 
-func interact(_player: Player):
+func interact(_player: Player): #Called when interact button pressed when focussed
 	interacted.emit()
 
-func start_focus(_player: Player):
+func start_focus(_player: Player): #Called when player moves into range
 	focus_start.emit()
 
-func end_focus(_player: Player):
+func end_focus(_player: Player): #Called when player moves out of range
 	focus_end.emit()
